a4c0b3b7c0baed7e46a5f137c19a41f6a0e3aaa5
rinkucool007 <pratap_the_knight@yahoo.com>
2023-10-11 22:38:35 -0400

    Update streaming.py

--- streaming.py
+++ streaming.py
@@ -39,3 +39,56 @@ python streaming.py
 
 
 http://<your_pi_ip>:8080/video
+
+--------------------------------------
+
+pip install flask
+
+
+from flask import Flask, render_template, Response
+import picamera
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def generate():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)  # Adjust the resolution as needed
+        camera.framerate = 30  # Adjust the framerate as needed
+
+        while True:
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+                stream.seek(0)
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
+                stream.seek(0)
+                stream.truncate()
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
+
+
+
+index.html
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Raspberry Pi Video Streaming</title>
+  </head>
+  <body>
+    <img src="{{ url_for('video_feed') }}" width="640" height="480">
+  </body>
+</html>
+
+
+python stream_video.py
+.
